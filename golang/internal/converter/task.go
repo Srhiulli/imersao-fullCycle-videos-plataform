31727395eac8b390d72bea5ef95b8c6d3e4d7269
Srhiulli <srhiulli@gmail.com>
2024-10-30 06:23:12 -0300

    add error

--- golang/internal/converter/task.go
+++ golang/internal/converter/task.go
@@ -1,6 +1,10 @@
 package converter
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"log/slog"
+	"time"
+)
 
 type VideoConverter struct{}
 
@@ -15,4 +19,18 @@ func (vc *VideoConverter) Handle(msg []byte) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func (vc *VideoConverter) logError(task VideoTask, message string, err error) {
+	errorData := map[string]any{ //valor pode ser de qualquer tipo === any
+		"video_id": task.videoID,
+		"error":    message,
+		"details":  err.Error(),
+		"time":     time.Now(),
+	}
+	serializedError, _ := json.Marshal(errorData)
+	slog.Error("Processing error", slog.String("error_details", string(serializedError)))
+
+	//TODO: register error on database
+
 }
